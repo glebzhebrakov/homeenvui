cce830a5a54864b996fffea0d7d967b2861b5922
Gleb Zhebrakov <gzhebrakov@sciforce.solutions>
2017-02-12 16:07:12 +0200

    Initial commit

---
+++ app/scripts/controllers/dashboardcontroller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name homeenvuiApp.controller:DashboardcontrollerCtrl
+ * @description
+ * # DashboardcontrollerCtrl
+ * Controller of the homeenvuiApp
+ */
+angular.module('homeenvuiApp')
+  .controller('DashboardcontrollerCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

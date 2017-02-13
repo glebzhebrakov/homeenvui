f19be2df5b171826058ec7d6bfe1095b8a671d47
Gleb Zhebrakov <gzhebrakov@sciforce.solutions>
2017-02-13 13:34:06 +0200

    images

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,17 @@
  * Controller of the homeenvApp
  */
 angular.module('homeenvApp')
-  .controller('MainCtrl',['$scope', function ($scope) {
-    $scope.test = "test";
+  .controller('MainCtrl',['$scope','$http', function ($scope, $http) {
+
+    $scope.clsimages = [];
+
+    $http({
+      method: 'GET',
+      url: '/rest/api/classifications/all'
+    }).then(function successCallback(response) {
+      console.log(response.data);
+      $scope.clsimages = response.data;
+    }, function errorCallback(response) {
+
+    });
   }]);

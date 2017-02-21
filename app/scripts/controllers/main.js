7e4f41707ca34b006691063522ab00c2802a5094
Gleb Zhebrakov <gzhebrakov@sciforce.solutions>
2017-02-21 18:52:54 +0200

    infinite scroll

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,15 +10,40 @@
 angular.module('homeenvApp')
   .controller('MainCtrl',['$scope','$http', function ($scope, $http) {
 
-    $scope.clsimages = [];
+    $scope.imagesview = [];
+    var lastload = 0;
+    // (function init() {
+    //   $scope.imagesview = [];
+    //   $http({
+    //     method: 'GET',
+    //     url: '/rest/api/classifications/0/20'
+    //   }).then(function successCallback(response) {
+    //     console.log(response.data);
+    //     $scope.imagesview = response.data;
+    //   }, function errorCallback(response) {
+    //     console.log(response);
+    //   });
+    // })();
 
-    $http({
-      method: 'GET',
-      url: '/rest/api/classifications/all'
-    }).then(function successCallback(response) {
-      console.log(response.data);
-      $scope.clsimages = response.data;
-    }, function errorCallback(response) {
-      console.log(response);
-    });
+
+
+    $scope.loadMore = function() {
+
+
+      var pagenum =  ((Math.round($scope.imagesview.length/20)));
+      if (lastload != pagenum || pagenum==0){
+        lastload = pagenum;
+        $http({
+          method: 'GET',
+          url: '/rest/api/classifications/'+pagenum+'/20'
+        }).then(function successCallback(response) {
+          console.log(response.data);
+          $scope.imagesview =  $scope.imagesview.concat(response.data);
+
+        }, function errorCallback(response) {
+          console.log(response);
+        });
+      }
+
+    };
   }]);
